d31004aa9c5ab17778d018c780f226fd174dd259
Muhammad-Irfan324 <52447969+Muhammad-Irfan324@users.noreply.github.com>
2020-12-23 21:58:57 +0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # AUTHOR - MUHAMMAD IRFAN TAHIR
 terraform {
   backend "gcs" {
-    bucket = "state-storage-bucket-addoptify-terraform"
+    bucket = "bucket-addoptify-terraform"
     prefix = "terraform/state"
   }
 }
